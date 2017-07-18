dc2d73a69f3f3b12ca2fc9ef100d4b17c936aa9f
iBeket <milos.milosavljevic21@gmail.com>
2017-07-18 17:44:58 +0200

    adding features to fragments, and some improvements

--- app/src/main/java/com/example/milos/vezba/FragmentActivity.java
+++ app/src/main/java/com/example/milos/vezba/FragmentActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import android.view.View;
 import android.widget.Button;
 
 
@@ -23,6 +24,8 @@ public class FragmentActivity extends AppCompatActivity {
 
     private Button photo;
     private Button video;
+    private Button change;
+    private boolean isClicked =false;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,34 +34,43 @@ public class FragmentActivity extends AppCompatActivity {
 
         photo = (Button) findViewById(R.id.button_fragment_photo);
         video = (Button) findViewById(R.id.button_fragment_video);
-
+        change = (Button) findViewById(R.id.change);
         //permissions for camera
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             photo.setEnabled(false);
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
         }
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+        FragmentClass1 frg1 = new FragmentClass1();
+        fragmentTransaction.replace(R.id.fragment_container, frg1);
+        fragmentTransaction.commit();
+        isClicked=true;
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isClicked) {
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    FragmentClass1 frg1 = new FragmentClass1();
+                    fragmentTransaction.replace(R.id.fragment_container, frg1);
+                    fragmentTransaction.commit();
+                    isClicked=true;
+                } else {
+                    FragmentManager fragmentManager1 = getFragmentManager();
+                    FragmentTransaction fragmentTransaction1 = fragmentManager1.beginTransaction();
 
-            FragmentClass1 frg1 = new FragmentClass1();
-            fragmentTransaction.replace(R.id.fragment_container, frg1);
-            fragmentTransaction.commit();
+                    FragmentClass2 frg2 = new FragmentClass2();
+                    fragmentTransaction1.replace(R.id.fragment_container, frg2);
+                    fragmentTransaction1.commit();
+                    isClicked=false;
+                }
+            }
+        });
 
-        } else {
-
-
-            FragmentManager fragmentManager1 = getFragmentManager();
-            FragmentTransaction fragmentTransaction1 = fragmentManager1.beginTransaction();
-
-            FragmentClass2 frg2 = new FragmentClass2();
-            fragmentTransaction1.replace(R.id.fragment_container, frg2);
-            fragmentTransaction1.commit();
-        }
     }
-
     //permissions for camera
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
